4d7c0984b192f25fdbcd9cd71b4d82cf8541021d
yaojiejia <yaojiejia0715@gmail.com>
2024-07-15 00:31:09 -0400

    update

--- server/routes/getIssuesRoute.ts
+++ server/routes/getIssuesRoute.ts
@@ -18,4 +18,35 @@ router.get("/issues", async (req:Request, res:Response) => {
 
 })
 
+// router.get("/issues", async (req:Request, res:Response) => {
+//     console.log('suceess!')
+
+//     try{
+//         const {tag, language} = req.query
+        
+
+//         const {result} = await findIssues(tag.toString(), language.toString());
+//         res.send(result)
+//     }
+//     catch(e){
+//         console.log(e)
+//     }
+
+// })
+
+// router.get("/issues", async (req:Request, res:Response) => {
+//     console.log('suceess!')
+
+//     try{
+//         const {tag, language} = req.query
+        
+
+//         const {result} = await findIssues(tag.toString(), language.toString());
+//         res.send(result)
+//     }
+//     catch(e){
+//         console.log(e)
+//     }
+
+// })
 export default router;
